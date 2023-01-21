5b1dd06b71b22b0a541b0599f895bb8177573795
Jordan O'Grady <jord_ogrady@hotmail.com>
2023-01-21 19:20:52 +1100

    questions and generate functions added, file generates off answers given

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,37 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return ""
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return ""
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return ""
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
-}
+# Description
+${data.Description}
+# Installation
+The following necessary dependencies must be installed to run the application properly: ${data.Installation}
+# Usage
+In order to use this app, ${data.Usage}
+# License
+${data.License}.
+# Credits
+​Contributors: ${data.Credit}
+# Tests
+The following is needed to run the test: ${data.Tests}
+`
+};
 
 module.exports = generateMarkdown;
